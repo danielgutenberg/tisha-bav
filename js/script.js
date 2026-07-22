426cbdea45d642e2391787e252b55b3909873036
Daniel Gutenberg <danielguten@gmail.com>
2026-07-22 18:03:19 +0300

    update keeper image

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@
     { title: "BRS", subtitle: "From Grudges to Geulah", img: "images/brs.jpg", desc: "", url: "https://content.jcn.io/brs9av", trailer: "" },
     { title: "BJX", subtitle: "Life Savers", img: "images/bjx.jpg", desc: "", url: "https://content.jcn.io/bjx9av", trailer: "" },
     { title: "Rabbi Yechiel Spero", subtitle: "The Siddur That Spoke", img: "images/yechiel-spero.jpg", desc: "", url: "https://content.jcn.io/alKr9Y", trailer: "https://d10xsoss226fg9.cloudfront.net/y0oJM89D8PKlGGzt8WsK1gaxE11LkpEp/F00F0B61022FC928F0340F2EE0784452/vl/2f84ec70f81846abbff9c47bb7766df8/TheSiddurThatSpokeofficialtrailer-1784232288.mp4" },
-    { title: "Toveedo", subtitle: "The Keeper of the Keilim", img: "images/toveedo.jpg", desc: "", url: "https://content.jcn.io/SjCSPN", trailer: "" },
+    { title: "Toveedo", subtitle: "The Keeper of the Keilim", img: "images/keeper.jpg", desc: "", url: "https://content.jcn.io/SjCSPN", trailer: "" },
     { title: "The Loop", subtitle: "Listen! Laugh! Learn!", img: "images/loop.jpg", desc: "", url: "https://content.jcn.io/A0fgVm", trailer: "" },
   ];
 
